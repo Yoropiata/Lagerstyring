fbd335a49737315a59a4526a15a920b9ea88cc28
Kristian Lind Kristensen <klkr.skp@edu.mercantec.dk>
2020-04-20 11:02:34 +0200

    Database Migrations

---
+++ database/migrations/2020_04_20_085223_create_inventory_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateInventoryTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('inventory', function (Blueprint $table) {
+            $table->increments('id')->unsigned();
+            $table->string("product_name", 100)->unique();
+            $table->integer("product_count")->unsigned();
+            $table->integer("department_id")->unsigned();
+
+            $table->foreign('department_id')->references('id')->on('department');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('inventory');
+    }
+}
