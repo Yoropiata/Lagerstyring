a915474b8f6f3c1075ba433e65fb4b24dceb05ec
Kristian Lind Kristensen <klkr.skp@edu.mercantec.dk>
2020-04-20 15:44:58 +0200

    Added user seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             DepartmentsTableSeeder::class,
             ProductsTableSeeder::class,
+            UsersTableSeeder::class,
         ]);
     }
 }
