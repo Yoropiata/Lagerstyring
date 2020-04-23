77a0172b24af82eb81796ba04f061dfcecc36378
Jack Jensen <numtjok1234@gmail.com>
2020-04-23 10:46:17 +0200

    Changed link

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -13,7 +13,7 @@
         <a class="nav-link" href="/inventar/ret">Ret Lager</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="/Admin">Admin</a>
+        <a class="nav-link" href="/brugere">Admin</a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="{{ url('/logout') }}">Log ud</a>
